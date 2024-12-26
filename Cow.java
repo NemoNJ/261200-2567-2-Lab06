772163eac91daaf6216d8a6050f6be79d47220fa
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2024-12-26 21:51:20 +0700

    Create Cow.java

---
+++ Cow.java
@@ -0,0 +1,14 @@
+public class Cow extends Animal{
+    @Override
+    void sound(){
+        System.out.println("MOO MOO");
+    }
+    void consume(Animal animal) {
+
+    }
+    void milk(){
+        System.out.println("...now loading...");
+    }
+}
+//public class Cow extends Animal{
+//} อันนี้ไม่ได้
